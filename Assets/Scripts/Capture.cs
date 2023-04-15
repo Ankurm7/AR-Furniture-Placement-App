88afa386b4152890c3641d8d5c5c61c6d498256b
Ankurm7 <ankurmallick07@gmail.com>
2023-04-15 22:47:09 +0530

    adding capture functionality

---
+++ Assets/Scripts/Capture.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class Capture : MonoBehaviour
+{
+    public void CaptureScreenshot()
+    {
+        StartCoroutine(TakeScreenshotAndSave());
+    }
+
+    private IEnumerator TakeScreenshotAndSave()
+    {
+        yield return new WaitForEndOfFrame();
+
+        Texture2D ss = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
+        ss.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
+        ss.Apply();
+
+        // Save the screenshot to Gallery/Photos
+        NativeGallery.Permission permission =
+            NativeGallery.SaveImageToGallery(ss, "GalleryTest", System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".png", (success, path) =>
+            Debug.Log("Media save result: " + success + " " + path));
+
+        Debug.Log("Permission result: " + permission);
+
+        // To avoid memory leaks
+        Destroy(ss);
+    }
+}
